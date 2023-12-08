ca09325ac7d9a0e512dc579c8ee3d07213ec425a
Carlos Quiala <cquiala12@gmail.com>
2023-12-08 17:48:17 -0500

    Fix Form v2.0

--- main.py
+++ main.py
@@ -186,10 +186,10 @@ async def root(request: Request, id_blog:str):
     return templates.TemplateResponse("blog.html", {"request": request, "content": content})
 
 @app.post("/sendform", tags=["contacto"])
-async def form(name: str = Form(min_length=1, max_length=100), age: int = Form(ge=0, le=100), 
-               email: EmailStr = Form(...), person_number: int = Form(ge=0, le=50), adult_number: int = Form(ge=0, le=50),
-               children_number: int = Form(ge=0, le=50), destiny: str = Form(min_length=1, max_length=100), duration: int = Form(ge=0, le=50),
-               start_date: str = Form(...), end_date:str = Form(...), experiences:str = Form(min_length=1, max_length=300),
+async def form(name: str = Form(...,min_length=1, max_length=100), age: int = Form(...,ge=0, le=100), 
+               email: EmailStr = Form(...), person_number: int = Form(...,ge=0, le=50), adult_number: int = Form(...,ge=0, le=50),
+               children_number: int = Form(...,ge=0, le=50), destiny: str = Form(...,min_length=1, max_length=100), duration: int = Form(...,ge=0, le=50),
+               start_date: str = Form(...), end_date:str = Form(...), experiences:str = Form(...,min_length=1, max_length=300),
                ):
     
     datos = { "name" : name, 
