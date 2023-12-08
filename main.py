9912d1ccbb4dbf596870d4192c6a268b253a9a1f
Carlos Quiala <cquiala12@gmail.com>
2023-12-08 14:17:49 -0500

    4

--- main.py
+++ main.py
@@ -13,22 +13,163 @@ app.mount("/assets", StaticFiles(directory="assets"), name="assets")
 templates = Jinja2Templates(directory="templates")
 
 
-
 @app.get("/", tags=["inicio"])
 async def root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/main", tags=["inicio"])
+async def root(request: Request):
+    return templates.TemplateResponse("indexenglish.html", {"request": request})
 
 @app.get("/destinocuba", tags=["cuba"])
 async def root(request: Request):
     return templates.TemplateResponse("destinocuba.html", {"request": request})
 
+@app.get("/destinocubaenglish", tags=["cuba"])
+async def root(request: Request):
+    return templates.TemplateResponse("destinocubaenglish.html", {"request": request})
+
 @app.get("/destinomexico", tags=["mexico"])
 async def root(request: Request):
     return templates.TemplateResponse("destinomexico.html", {"request": request})
 
+@app.get("/destinomexicoenglish", tags=["mexico"])
+async def root(request: Request):
+    return templates.TemplateResponse("destinomexicoenglish.html", {"request": request})
+
+
 @app.get("/contacto", tags=["inicio"])
 async def root(request: Request):
     return templates.TemplateResponse("contacto.html", {"request": request})
+
+@app.get("/contactoenglish", tags=["inicio"])
+async def root(request: Request):
+    return templates.TemplateResponse("contactoenglish.html", {"request": request})
+
+@app.get("/arquitecturacuba", tags=["inicio"])
+async def root(request: Request):
+    return templates.TemplateResponse("arquitecturacuba.html", {"request": request})
+
+@app.get("/arquitecturacubaenglish", tags=["inicio"])
+async def root(request: Request):
+    return templates.TemplateResponse("arquitecturacubaenglish.html", {"request": request})
+
+@app.get("/arquitecturayucatan", tags=["inicio"])
+async def root(request: Request):
+    return templates.TemplateResponse("arquitecturayucatan.html", {"request": request})
+
+@app.get("/arquitecturayucatanenglish", tags=["inicio"])
+async def root(request: Request):
+    return templates.TemplateResponse("arquitecturayucatanenglish.html", {"request": request})
+
+@app.get("/artecuba", tags=["inicio"])
+async def root(request: Request):
+    return templates.TemplateResponse("artecuba.html", {"request": request})
+
+@app.get("/artecubaenglish", tags=["inicio"])
+async def root(request: Request):
+    return templates.TemplateResponse("artecubaenglish.html", {"request": request})
+
+@app.get("/arteyucatan", tags=["inicio"])
+async def root(request: Request):
+    return templates.TemplateResponse("arteyucatan.html", {"request": request})
+
+@app.get("/arteyucatanenglish", tags=["inicio"])
+async def root(request: Request):
+    return templates.TemplateResponse("arteyucatanenglish.html", {"request": request})
+
+@app.get("/bienestaryucatan", tags=["inicio"])
+async def root(request: Request):
+    return templates.TemplateResponse("bienestaryucatan.html", {"request": request})
+
+@app.get("/bienestaryucatanenglish", tags=["inicio"])
+async def root(request: Request):
+    return templates.TemplateResponse("bienestaryucatanenglish.html", {"request": request})
+
+@app.get("/blog", tags=["inicio"])
+async def root(request: Request):
+    return templates.TemplateResponse("blog.html", {"request": request})
+
+@app.get("/cenotesyucatan", tags=["inicio"])
+async def root(request: Request):
+    return templates.TemplateResponse("cenotesyucatan.html", {"request": request})
+
+@app.get("/cenotesyucatanenglish", tags=["inicio"])
+async def root(request: Request):
+    return templates.TemplateResponse("cenotesyucatanenglish.html", {"request": request})
+
+@app.get("/ecoturismoyucatan", tags=["inicio"])
+async def root(request: Request):
+    return templates.TemplateResponse("ecoturismoyucatan.html", {"request": request})
+
+@app.get("/ecoturismoyucatanenglish", tags=["inicio"])
+async def root(request: Request):
+    return templates.TemplateResponse("ecoturismoyucatanenglish.html", {"request": request})
+
+@app.get("/fotografiacuba", tags=["inicio"])
+async def root(request: Request):
+    return templates.TemplateResponse("fotografiacuba.html", {"request": request})
+
+@app.get("/fotografiacubaenglish", tags=["inicio"])
+async def root(request: Request):
+    return templates.TemplateResponse("fotografiacubaenglish.html", {"request": request})
+
+@app.get("/gastronomiayucatan", tags=["inicio"])
+async def root(request: Request):
+    return templates.TemplateResponse("gastronomiayucatan.html", {"request": request})
+
+@app.get("/gastronomiayucatanenglish", tags=["inicio"])
+async def root(request: Request):
+    return templates.TemplateResponse("gastronomiayucatanenglish.html", {"request": request})
+
+@app.get("/haciendasyucatan", tags=["inicio"])
+async def root(request: Request):
+    return templates.TemplateResponse("haciendasyucatan.html", {"request": request})
+
+@app.get("/haciendasyucatanenglish", tags=["inicio"])
+async def root(request: Request):
+    return templates.TemplateResponse("haciendasyucatanenglish.html", {"request": request})
+
+@app.get("/indexgaleria", tags=["inicio"])
+async def root(request: Request):
+    return templates.TemplateResponse("indexgaleria.html", {"request": request})
+
+@app.get("/indexgaleriaenglish", tags=["inicio"])
+async def root(request: Request):
+    return templates.TemplateResponse("indexgaleriaenglish.html", {"request": request})
+
+@app.get("/peopleyucatan", tags=["inicio"])
+async def root(request: Request):
+    return templates.TemplateResponse("peopleyucatan.html", {"request": request})
+
+@app.get("/peopleyucatanenglish", tags=["inicio"])
+async def root(request: Request):
+    return templates.TemplateResponse("peopleyucatanenglish.html", {"request": request})
+
+@app.get("/religioncuba", tags=["inicio"])
+async def root(request: Request):
+    return templates.TemplateResponse("religioncuba.html", {"request": request})
+
+@app.get("/religioncubaenglish", tags=["inicio"])
+async def root(request: Request):
+    return templates.TemplateResponse("religioncubaenglish.html", {"request": request})
+
+@app.get("/shoppingcuba", tags=["inicio"])
+async def root(request: Request):
+    return templates.TemplateResponse("shoppingcuba.html", {"request": request})
+
+@app.get("/shoppingcubaenglish", tags=["inicio"])
+async def root(request: Request):
+    return templates.TemplateResponse("shoppingcubaenglish.html", {"request": request})
+
+@app.get("/vidanocturnayucatan", tags=["inicio"])
+async def root(request: Request):
+    return templates.TemplateResponse("vidanocturnayucatan.html", {"request": request})
+
+@app.get("/vidanocturnayucatanenglish", tags=["inicio"])
+async def root(request: Request):
+    return templates.TemplateResponse("vidanocturnayucatanenglish.html", {"request": request})
+
 
 @app.get("/blogs/{id_blog}", tags=["blogs"])
 async def root(request: Request, id_blog:str):
